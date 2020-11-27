3420a94d5bffade287bcfb90ffb3217db8d37f3b
Tony Carvalho <tonylcbarros@gmail.com>
2020-11-27 18:13:25 -0300

    adicionando estudo de javascript

---
+++ modulo1-javascript.js
@@ -0,0 +1,29 @@
+// usa-se 'var' para armazenar valores
+
+var todos = "valores armazenados"
+
+// mostrar saídas sobre o código
+
+console.log("a frase armazenada é" + todos)
+
+// dar entrada de texto / receber um input do usuário
+
+var entrada = prompt("insira um valor")
+
+
+// NÚMEROS E OPERADORES
+
+var nome = "Tony";
+var sobrenome ="Carvalho";
+var nomeCompleto = nome + ' ' sobrenome;
+
+console.log(nomeCompleto);
+
+
+// CONDICIONAIS
+
+var nome = "Tony"
+
+if (nome === "Tony") {
+    console.log('Legal! Seu nome é este mesmo')
+}
